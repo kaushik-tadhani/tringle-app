5344a283f916fe383b54851573f6ea9312c4839d
Kaushik Tadhani <tadkaushik@gmail.com>
2024-02-12 19:26:07 -0500

    Add project files.

---
+++ TringleApp/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+
+namespace TringleApp
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
---
+++ TringleAppTest/TriangleTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using TringleApp;
+
+namespace TringleAppTest
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        // Kaushik Tadhani
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTrinagle()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            string expected = "The triangle is valid.";
+
+            // Act
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
